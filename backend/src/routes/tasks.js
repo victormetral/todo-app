38d7489b3df1dee4f376aff9f1ef130726a63d29
victormetral <victor.metral@gmail.com>
2026-06-23 16:40:08 +0200

    Ajoute les routes Express pour l'API tasks

---
+++ backend/src/routes/tasks.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { getAllTasks, createTask, updateTask, deleteTask } = require('../controllers/tasks');
+
+router.get('/', getAllTasks);
+router.post('/', createTask);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
+
+module.exports = router;
